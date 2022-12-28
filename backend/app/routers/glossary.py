1f4c2ec183f5f9f231f505486b5cfaf9b41d3827
skachkovkirill <skachkovkiril@yandex.ru>
2022-12-28 22:35:57 +0300

    Added the ability to search by terms

--- backend/app/routers/glossary.py
+++ backend/app/routers/glossary.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..utils.node import get_nodes
-from ..utils.glossary import get_term, get_glossary
+from ..utils.glossary import get_term, get_glossary, search_term
 from ..schemas.glossary import BaseGlossary, Definition
 from fastapi import HTTPException
 
@@ -16,6 +16,11 @@ router = APIRouter(
 @router.get('/list_of_dict', response_model=list[BaseGlossary])
 async def term_definition(db: Session = Depends(get_db)):
     return get_nodes(db)
+
+
+@router.get('/list_of_dict/{key}', response_model=list[BaseGlossary])
+async def search(key: str, db: Session = Depends(get_db)):
+    return search_term(key=key, db=db)
 
 
 @router.get('', response_model=dict[str, str])
--- backend/app/utils/glossary.py
+++ backend/app/utils/glossary.py
@@ -10,3 +10,8 @@ def get_glossary(db: Session):
 def get_term(term: str, db: Session):
     """ Возвращает опредедение по термину """
     return db.query(Node).filter(Node.term == term).first()
+
+
+def search_term(key: str, db: Session):
+    """ Возвращает термины с определениями, содержащими ключевое слово """
+    return db.query(Node).filter(Node.term.ilike(f'%{key}%')).all()
