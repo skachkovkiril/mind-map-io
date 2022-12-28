08aedf9252cd0a6e75e8b8069dba0952846802c1
skachkovkirill <skachkovkiril@yandex.ru>
2022-12-28 22:41:49 +0300

    Added the ability to search for a term (front-end)

--- frontend/src/utils/get_glossary.ts
+++ frontend/src/utils/get_glossary.ts
@@ -6,3 +6,16 @@ export function getGlossary() {
   }).then((data) => data.json());
   return response;
 }
+
+export function getTerm(term: string) {
+  const headersList = { Accept: "*/*" };
+  const response = fetch(
+    `http://127.0.0.1:8000/glossary/list_of_dict/${term}`,
+    {
+      method: "GET",
+      headers: headersList,
+    }
+  ).then((data) => data.json());
+
+  return response;
+}
--- frontend/src/views/GlossaryView.vue
+++ frontend/src/views/GlossaryView.vue
@@ -1,35 +1,53 @@
 <template>
   <div class="home">
-    <div
-      v-for="item in response_data"
-      :key="item['term']"
-      class="card text-bg-secondary mb-3"
-      style="max-width: 100%"
-    >
-      <div class="card-header">{{ item["term"] }}</div>
-      <div class="card-body">
-        <p class="card-text">
-          {{ item["definition"] }}
-        </p>
+    <form class="my-3" v-on:submit.prevent>
+      <input
+        type="search"
+        v-model="term"
+        class="form-control form-control-dark"
+        placeholder="Поиск..."
+        aria-label="Поиск"
+        v-on:keyup.enter="search"
+      />
+    </form>
+    <div v-if="response_data.length != 0">
+      <div
+        v-for="item in response_data"
+        :key="item['term']"
+        class="card text-bg-secondary mb-3"
+        style="max-width: 100%"
+      >
+        <div class="card-header">{{ item["term"] }}</div>
+        <div class="card-body">
+          <p class="card-text">
+            {{ item["definition"] }}
+          </p>
+        </div>
       </div>
     </div>
+    <div v-else>Данные не найдены</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { getGlossary } from "@/utils/get_glossary";
+import { getGlossary, getTerm } from "@/utils/get_glossary";
 
 export default defineComponent({
   name: "GlossaryView",
-  components: {},
   data() {
     return {
-      response_data: JSON,
+      response_data: [],
+      term: "",
     };
   },
   async mounted() {
     this.response_data = await getGlossary();
+  },
+  methods: {
+    search: async function () {
+      this.response_data = await getTerm(this.term);
+    },
   },
 });
 </script>
